12c732d00a5866afe2a9809c4be95c0fb48099e3
Rik Balvers <rikbalvers@outlook.com>
2023-10-04 12:45:08 +0200

    rol update met spatie

--- verzamelaarsplatform/database/migrations/2014_08_12_000000_create_users_table.php
+++ verzamelaarsplatform/database/migrations/2014_08_12_000000_create_users_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->constrained('roles')->default('1');
-            $table->string('email')->unique();
             $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->timestamp('request_at');
-            $table->timestamp('verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
