2a9a9d77a253ddc647e386d8f96c01a245d73261
Rik Balvers <rikbalvers@outlook.com>
2023-10-05 15:14:56 +0200

    update roles permissions

--- verzamelaarsplatform/resources/views/admin/permissions/index.blade.php
+++ verzamelaarsplatform/resources/views/admin/permissions/index.blade.php
@@ -3,7 +3,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-2">
                 <div class="flex justify-end p-2">
-                    <a href="#" class="px-4 py-2 bg-green-700 hover:bg-green-500 rounded-md">Create Permission</a>
+                    <a href="{{ route('admin.permissions.create') }}" class="px-4 py-2 bg-green-700 hover:bg-green-600 text-slate-100 rounded-md">Create Permission</a>
                 </div>
                 <div class="flex flex-col">
                     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -27,8 +27,12 @@
                                                 </div>
                                             </td>
                                             <td>
-                                                <a href="#">Edit</a>
-                                                <a href="#">Delete</a>
+                                                <div class="flex justify-end">
+                                                    <div class="space-x-1">
+                                                        <a href="{{ route('admin.permissions.edit', $permission->id) }}" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md">Edit</a>
+                                                        <a href="" class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded-md">Delete</a>
+                                                    </div>
+                                                </div>
                                             </td>
                                         </tr>  
                                         @endforeach
