282716726256982991c78e3d680facea038336ef
Rik Balvers <rikbalvers@outlook.com>
2023-11-10 11:10:40 +0100

    permissie en acc

--- verzamelaarsplatform/database/seeders/PermissionSeeder.php
+++ verzamelaarsplatform/database/seeders/PermissionSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class PermissionSeeder extends Seeder
 {
@@ -13,11 +14,33 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        Permission::create([
+        $permission1 = Permission::create([
             'name' => 'Gebruikers beheren',
-            'email' => 'admin@admin.com',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        ])->assignRole('admin');
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        $permission2 = Permission::create([
+            'name' => 'Collectie beheren',
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        $permission3 = Permission::create([
+            'name' => 'Klantportaal beheren',
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // Assign all permissions to the 'admin' role
+        $adminRole = Role::where('name', 'admin')->first();
+        $adminRole->givePermissionTo([$permission1, $permission2, $permission3]);
+
+        // Assign 'Collectie beheren' permission to the 'user' role
+        $userRole = Role::where('name', 'user')->first();
+        $userRole->givePermissionTo($permission2);
     }
 }
