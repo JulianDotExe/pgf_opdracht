d9d55d42c23fec36f11ccd04fe7c676f6df6938a
Julian van Stavel <julianvstavel@gmail.com>
2023-11-14 12:13:47 +0100

    Test

--- verzamelaarsplatform/resources/views/about.blade.php
+++ verzamelaarsplatform/resources/views/about.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Homepage</title>
+        <title>About me</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -26,33 +26,29 @@
         <header class="z-50 absolute">
         <!-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"> -->
             @if (Route::has('login'))
-            
-                <div class="fixed sm:top-0 sm:right-0 p-9 text-center z-5 bg-[#1F2937] w-full border-none">
-                    <div class="bg-[url('../img/TrainLogo.png')] top-0 left-5px z-2 fixed w-20 h-20 bg-no-repeat bg-[length:4rem_4rem]"></div>     
-                    <!-- <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log sdsin</a>      -->
-                        <div class="fixed sm:top-0 sm:right-0 p-6 w-full border-none text-right">         
+                <div class="fixed sm:top-0 sm:right-0 p-6 text-right z-5 bg-[#1F2937] w-full border-none">
+                    <div class="bg-[url('../img/TrainLogo.png')] top-0 left-5px z-2 fixed w-20 h-20 bg-no-repeat bg-[length:4rem_4rem]"></div>                   
                      @auth
                         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">De Leeuw</a>
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Collections</a>
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">News</a>
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Events</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
                         @endif
                     @endauth
-                    </div>
                 </div>
             @endif
         </header>
         
-
-
         <head-content>
-            <div class="bg-[url('../img/Background.png')] w-full h-screen  bg-no-repeat bg-cover -z-1"></div>
-                <div class="text-container absolute left-1/3 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-10">
-                    <p class="text-white text-6xl">Welkom</p><br>
-                    <p class="text-white text-3xl mt-2">Waar onze liefde en passie<br> voor modeltreinen samenkomt!</p>
-                </div>
+            <div class="relative">
+                <div class="bg-[url('../img/Background.png')] w-full h-screen  bg-no-repeat bg-cover"></div>
+            </div>
         </head-content>
 
         <!-- <main-content>
