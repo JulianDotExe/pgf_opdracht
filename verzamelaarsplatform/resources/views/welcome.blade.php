ce562d72a128547b5a82433a0429d8e4fc5639eb
Rik Balvers <rikbalvers@outlook.com>
2023-11-15 09:37:17 +0100

    Update welcome.blade.php

--- verzamelaarsplatform/resources/views/welcome.blade.php
+++ verzamelaarsplatform/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
                 </div>
 
                 <!-- Login/Register Section -->
-                <div class="flex items-center">                   
+                <div class="flex items-center">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                     @else
