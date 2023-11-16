570062ea7f3403537d7b72dafe20f335926a9bd3
Rik Balvers <rikbalvers@outlook.com>
2023-11-16 14:14:47 +0100

    update mail

--- verzamelaarsplatform/resources/views/mail/new_user_registered.blade.php
+++ verzamelaarsplatform/resources/views/mail/new_user_registered.blade.php
@@ -9,13 +9,21 @@
     <p>Beste beheerder,</p>
 
     <p>Er is een nieuwe gebruiker geregistreerd op ons platform:</p>
-    
+
     <ul>
         <li>Naam: {{ $data->name }}</li>
         <li>E-mail: {{ $data->email }}</li>
     </ul>
 
-    <p>Om deze registratie te accepteren of beheren, kun je inloggen op het beheerdersgedeelte van ons platform en de benodigde acties ondernemen.</p>
+    <p>Om deze registratie te accepteren of beheren, kun je inloggen op het beheerdersgedeelte van ons platform en de benodigde acties ondernemen:</p>
+    <ol>
+        <li>Log in als beheerder</li>
+        <li>Bekijk het beheermodule</li>
+        <li>Klik op Users in het menu</li>
+        <li>Klik op roles van de nieuwe gebruiker</li>
+        <li>Geef aan wat voor rol de nieuwe gebruiker mag hebben</li>
+        <li>Klik op assign</li>
+    </ol>
     
     <p>Vriendelijke groeten,<br> Onze verzamelaarsplatform</p>
 </body>
