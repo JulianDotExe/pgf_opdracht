235e40472a0a75a1ef4adccbe439e7db3134d457
Rik Balvers <rikbalvers@outlook.com>
2023-11-16 13:35:27 +0100

    Update web.php

--- verzamelaarsplatform/routes/web.php
+++ verzamelaarsplatform/routes/web.php
@@ -35,9 +35,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
-
-
 Route::middleware(['auth', 'role:admin'])->name('admin.')->prefix('admin')->group(function() {
     Route::get('/', [IndexController::class, 'index'])->name('index');
     Route::resource('/roles', RoleController::class);
@@ -72,7 +69,6 @@ Route::get('/welcome', function () {
         Auth::logout();
         return redirect('/login'); // Redirect to the login page or any other page
     }
-
     return view('welcome');
 });
 
