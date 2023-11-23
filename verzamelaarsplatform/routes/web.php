04d0d9e7875d53a3bc624b935f987a15d0cab115
Ninoh van Dijke <47174@hoornbeeck.nl>
2023-11-23 11:37:16 +0100

    NEWS

--- verzamelaarsplatform/routes/web.php
+++ verzamelaarsplatform/routes/web.php
@@ -64,7 +64,7 @@ Route::middleware(['auth', 'role:admin'])->name('admin.')->prefix('admin')->grou
     Route::get('/news', [NewsController::class,'index'])->name('news.index');
     Route::get('/news/create', [NewsController::class, 'create'])->name('news.create');
     Route::post('/news', [NewsController::class, 'store'])->name('news.store');
-    // Route::get('/admin/news/{news}', [NewsController::class, 'show'])->name('admin.news.show');
+    Route::get('/news/{news}', [NewsController::class, 'show'])->name('news.show');
 
     Route::delete('/news/{news}', [NewsController::class, 'destroy'])->name('news.destroy');
 
