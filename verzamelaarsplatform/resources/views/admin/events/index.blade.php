e4b90f767801000110987f9be9930e4bd3129fc9
Rik Balvers <rikbalvers@outlook.com>
2023-11-24 09:19:58 +0100

    oh

--- verzamelaarsplatform/resources/views/admin/events/index.blade.php
+++ verzamelaarsplatform/resources/views/admin/events/index.blade.php
@@ -9,8 +9,9 @@
             <a href="{{ route('admin.events.create') }}" class="bg-blue-500 text-white p-3 rounded inline-block hover:bg-blue-600 transition duration-300 ease-in-out">Evenement toevoegen</a>
             @forelse($events as $event)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
-                    <h2><strong>Event name:</strong>{{ $event->event_name }}</h2>
-                    <p><strong>Date:</strong> {{ $event->event_date }}</p>
+                    <h2><strong>Event name: </strong>{{ $event->event_name }}</h2>
+                    <p><strong>Date: </strong> {{ $event->event_date }}</p>
+                    <p><strong>Categorie:</strong> {{ $event->category()->category_name }}</p>
 
                     <!-- Verwijderknop -->
                     <form method="POST" action="{{ route('admin.events.destroy', $event->event_id) }}" class="inline" onsubmit="return confirm('Are you sure you want to delete this event?')">
