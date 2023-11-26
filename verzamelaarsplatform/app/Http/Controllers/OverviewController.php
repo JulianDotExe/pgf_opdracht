4bf60124cdea1f2ae3bf1a6c5fcf8028e2a7a1db
Rik Balvers <rikbalvers@outlook.com>
2023-11-26 04:00:14 +0100

    Update OverviewController.php

--- verzamelaarsplatform/app/Http/Controllers/OverviewController.php
+++ verzamelaarsplatform/app/Http/Controllers/OverviewController.php
@@ -26,9 +26,6 @@ class OverviewController extends Controller
         $overviewsQuery = $user->overviews(); // Haal de overzichten van de gebruiker op
     
         // Search functionality on multiple columns
-        $overviewsQuery = Overview::query();
-
-        // Zoekfunctionaliteit op e-mail
         if ($request->filled('search')) {
             $searchTerm = $request->input('search');
             $overviewsQuery->where(function ($query) use ($searchTerm) {
@@ -37,18 +34,6 @@ class OverviewController extends Controller
                 })
                 ->orWhereHas('brand', function ($subQuery) use ($searchTerm) {
                     $subQuery->where('brand_name', 'like', "%$searchTerm%");
-                })
-                ->orWhereHas('owner', function ($subQuery) use ($searchTerm) {
-                    $subQuery->where('owner_name', 'like', "%$searchTerm%");
-                })
-                ->orWhereHas('epoche', function ($subQuery) use ($searchTerm) {
-                    $subQuery->where('epoche_name', 'like', "%$searchTerm%");
-                })
-                ->orWhereHas('color1', function ($subQuery) use ($searchTerm) {
-                    $subQuery->where('color1', 'like', "%$searchTerm%");
-                })
-                ->orWhereHas('color2', function ($subQuery) use ($searchTerm) {
-                    $subQuery->where('color2', 'like', "%$searchTerm%");
                 })
                 // Add similar clauses for other relationships...
                 ->orWhere('catalogusnr', 'like', "%$searchTerm%")
@@ -62,7 +47,6 @@ class OverviewController extends Controller
     
         return view('users.overviews.index', compact('overviews'));
     }
-    
 
     /**
      * Show the form for creating a new resource.
