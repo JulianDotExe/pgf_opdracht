a02055319f67f531fecedea254a13c240b4a9fc7
Julian van Stavel <julianvstavel@gmail.com>
2023-11-28 10:12:47 +0100

    Small changes

--- verzamelaarsplatform/resources/views/admin/overviews/index.blade.php
+++ verzamelaarsplatform/resources/views/admin/overviews/index.blade.php
@@ -18,10 +18,10 @@
 
             @forelse($overviews as $overview)
                 <div class="flex my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
-                    <div class="w-2/3 pr-6">
-                        <h2><strong>Eigenaar: </strong> {{ $overview->owner->owner_name}}</h2>
-                        <h2><strong>Soort: </strong> {{ $overview->sort->sort_name}}</h2>
-                        <p><strong>Merk: </strong> {{ $overview->brand->brand_name }}</p>
+                        <div class="w-2/3 pr-6">
+                            <h2><strong>Eigenaar: </strong> {{ $overview->owner->owner_name}}</h2>
+                            <h2><strong>Soort: </strong> {{ $overview->sort->sort_name}}</h2>
+                            <p><strong>Merk: </strong> {{ $overview->brand->brand_name }}</p>
                         <h1 class="pt-2">
                             <p><a href="{{ route('admin.overviews.show', $overview->id) }}" class="text-blue-500 hover:underline hover:text-blue-700 transition duration-300 ease-in-out">Meer details</a></p>
                             <!-- Verwijderknop -->
@@ -36,15 +36,20 @@
                         <div>
                             <div class="mt-2">
                                 @foreach($overview->getImages() as $image)
-                                    <img src="{{ asset($image) }}" alt="Collection Image" class="my-4 small-image">
+                                <img src="{{ asset($image) }}" alt="Collection Image" class="my-4 small-image" onclick="openLightbox('{{ asset($image) }}')">
                                 @endforeach
                             </div>
                         </div>
-                            @else
-                                <p>There's no image associated with this collection.</p>
-                            @endif
-                        </div>
+                        @else
+                        <p>There's no image associated with this collection.</p>
+                        @endif
                     </div>
+
+                    <!-- Lightbox container -->
+                    <div id="lightbox" class="lightbox" onclick="closeLightbox()">
+                        <img id="lightboxImage" src="" alt="Enlarged Image">
+                    </div>
+                </div>
 
                 <!-- Confirmation popup -->
                 <div id="confirmationPopup" class="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex items-center justify-center" style="display: none;">
@@ -75,16 +80,16 @@
 </x-app-layout>
 
 <script>
-    function deleteOpenConfirmationPopup(itemId) {
-        // Set the form action dynamically based on the item ID
-        var form = document.getElementById('deleteForm');
-        form.action = "{{ route('admin.overviews.destroy', '') }}" + '/' + itemId;
-        // Show the confirmation popup
-        document.getElementById('confirmationPopup').style.display = 'flex';
+    function openLightbox(imageSrc) {
+        // Set the lightbox image source
+        document.getElementById('lightboxImage').src = imageSrc;
+        // Show the lightbox
+        document.getElementById('lightbox').style.display = 'flex';
     }
-    function deleteCloseConfirmationPopup() {
-        // Hide the confirmation popup
-        document.getElementById('confirmationPopup').style.display = 'none';
+
+    function closeLightbox() {
+        // Hide the lightbox
+        document.getElementById('lightbox').style.display = 'none';
     }
 </script>
 
@@ -92,5 +97,25 @@
     .small-image {
         max-width: 100px; /* Adjust the max-width as needed */
         margin-left: auto; /* Move the image to the right */
+        cursor: pointer; /* Add cursor */
+    }
+
+    /* Lightbox styles */
+    .lightbox {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.7);
+        justify-content: center;
+        align-items: center;
+    }
+
+    .lightbox img {
+        max-width: 80%;
+        max-height: 80%;
+        border-radius: 5px;
     }
 </style>
